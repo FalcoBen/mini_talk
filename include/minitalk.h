c4ebb438584744595f706e4332b3b303372fe8b5
Fahd Benallal <fbenalla@e2r9p15.1337.ma>
2025-03-01 14:19:09 +0000

    some changes

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,16 +6,17 @@
 /*   By: fbenalla <fbenalla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 21:25:41 by fbenalla          #+#    #+#             */
-/*   Updated: 2025/02/28 23:51:22 by fbenalla         ###   ########.fr       */
+/*   Updated: 2025/03/01 14:18:36 by fbenalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef MINITALK_H
+# define MINITALK_H
 
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 typedef struct s_t
 {
@@ -28,6 +29,5 @@ typedef struct s_t
 int		ft_which_byte(unsigned char buffer[4]);
 void	ft_putnbr(int nbr);
 void	*ft_memset(void *b, int c, size_t len);
-void	do_(t_t *set, unsigned char *buffer);
-void	check_pid(t_t *set, int *si_pid, unsigned char *buffer);
-void	handler(int signal, siginfo_t *info, void *context);
+
+#endif
